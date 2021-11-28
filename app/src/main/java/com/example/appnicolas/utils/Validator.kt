9037cd45c642d464b2674c99c766aca5097c0d37
JNicoVM <jnvmud@gmail.com>
2021-11-28 00:09:37 -0500

    Adding two instruments test to valid url

--- app/src/main/java/com/example/appnicolas/utils/Validator.kt
+++ app/src/main/java/com/example/appnicolas/utils/Validator.kt
@@ -1,15 +1,21 @@
 package com.example.appnicolas.utils
 
-import android.webkit.URLUtil
+import kotlin.math.PI
+
 
 object Validator {
 
      fun isValidUrl (url:String): Boolean {
         return if(url.isNotEmpty()){
-            URLUtil.isValidUrl(url)
+            android.webkit.URLUtil.isValidUrl(url)
         }else{
             false
         }
+    }
+
+    fun isValidEmail(tet:String) :Boolean{
+        val a = PI
+        return a==0.0
     }
 
     fun buildValidUrl (url: String) : String {
